e29e369ffd57750b1ea16f523a520461e2981ba3
Augusto <augustodesouzacardoso@gmail.com>
2023-05-12 17:49:55 -0300

    Adiciona coluna post_img ao post

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from blog.models import Post
 
 
 def home(request):
-    posts = Post.objects.all()
+    posts = reversed(Post.objects.all())
     return render(request, 'home.html', {'posts': posts})
 
 
--- blog_musical/settings.py
+++ blog_musical/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import URLPattern
+
+if settings.DEBUG:
+    URLPattern += static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
